2a89767ca1475911980ad08404aeab4ec93ab1c0
Tom McLaughlin <ttom.mclaughlin@gmail.com>
2020-11-30 15:16:12 -0600

    Use ssh-keypair

--- ec2/main.tf
+++ ec2/main.tf
@@ -57,19 +57,32 @@ data "aws_ami" "amazon_linux" {
 }
 
 ################################################################################
+# Create SSH Keypair
+################################################################################
+resource "aws_key_pair" "master_key" {
+  key_name   = "${data.aws_vpc.selected.tags["Name"]}-master-key"
+  public_key = var.ec2_ssh_key_pub
+
+  tags = {
+    "Environment" = "dev"
+  }
+}
+
+################################################################################
 # Create a bastion host, place it in all public subnets
 ################################################################################
 module "ec2_bastion_host" {
-  depends_on     = [module.bastion_host_sg]
+  depends_on     = [module.bastion_host_sg, aws_key_pair.master_key]
   source         = "github.com/terraform-aws-modules/terraform-aws-ec2-instance?ref=v2.15.0"
   instance_count = 1
 
-  name                        = "bastion"
+  name                        = "${data.aws_vpc.selected.tags["Name"]}-bastion"
   ami                         = data.aws_ami.amazon_linux.id
   instance_type               = "t2.micro"
   subnet_ids                  = data.aws_subnet_ids.public.ids
   vpc_security_group_ids      = [module.bastion_host_sg.this_security_group_id]
   associate_public_ip_address = true
+  key_name                    = aws_key_pair.master_key.key_name
 
   tags = {
     "Environment" = "dev"
