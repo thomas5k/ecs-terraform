797ccd7ae76cf402312daa8c4ffeb91e394e865f
Tom McLaughlin <ttom.mclaughlin@gmail.com>
2020-11-30 14:39:57 -0600

    Bastion ec2 and sg creation

--- ec2/main.tf
+++ ec2/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 ################################################################################
-# Lookups for VPC, Subnets
+# Lookup VPC
 ################################################################################
 data "aws_vpc" "selected" {
   filter {
@@ -12,6 +12,9 @@ data "aws_vpc" "selected" {
   }
 }
 
+################################################################################
+# Look up subnets
+################################################################################
 data "aws_subnet_ids" "private" {
   vpc_id = data.aws_vpc.selected.id
 
@@ -29,5 +32,67 @@ data "aws_subnet_ids" "public" {
 }
 
 ################################################################################
-# Create a bastion host
+# Look up latest Amazon Linux AMI
 ################################################################################
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name = "name"
+
+    values = [
+      "amzn-ami-hvm-*-x86_64-gp2",
+    ]
+  }
+
+  filter {
+    name = "owner-alias"
+
+    values = [
+      "amazon",
+    ]
+  }
+}
+
+################################################################################
+# Create a bastion host, place it in all public subnets
+################################################################################
+module "ec2_bastion_host" {
+  depends_on     = [module.bastion_host_sg]
+  source         = "github.com/terraform-aws-modules/terraform-aws-ec2-instance?ref=v2.15.0"
+  instance_count = 1
+
+  name                        = "bastion"
+  ami                         = data.aws_ami.amazon_linux.id
+  instance_type               = "t2.micro"
+  subnet_ids                  = data.aws_subnet_ids.public.ids
+  vpc_security_group_ids      = [module.bastion_host_sg.this_security_group_id]
+  associate_public_ip_address = true
+
+  tags = {
+    "Environment" = "dev"
+    "Tier"        = "public"
+    "Type"        = "bastion"
+  }
+}
+
+################################################################################
+# Security Group For Bastion Host
+################################################################################
+module "bastion_host_sg" {
+  source = "github.com/terraform-aws-modules/terraform-aws-security-group/modules/ssh"
+
+  name            = "ssh"
+  use_name_prefix = false
+  vpc_id          = data.aws_vpc.selected.id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  tags = {
+    "Environment" = "dev"
+    "Tier"        = "public"
+    "Type"        = "bastion"
+  }
+}
--- ec2/outputs.tf
+++ ec2/outputs.tf
@@ -1,3 +1,7 @@
+
+################################################################################
+# VPC Info
+################################################################################
 output "vpc_arn" {
   description = "The ARN of the VPC used"
   value       = data.aws_vpc.selected.arn
@@ -8,6 +12,9 @@ output "vpc_id" {
   value       = data.aws_vpc.selected.id
 }
 
+################################################################################
+# Subnet Info
+################################################################################
 output "private_subnet_ids" {
   description = "The private the subnets in the VPC"
   value       = data.aws_subnet_ids.private.ids
@@ -16,4 +23,40 @@ output "private_subnet_ids" {
 output "public_subnet_ids" {
   description = "The public the subnets in the VPC"
   value       = data.aws_subnet_ids.public.ids
+}
+
+################################################################################
+# Bastion Host Info
+################################################################################
+output "ec2_bastion_host_id" {
+  description = "ID of the Bastion Host for this VPC"
+  value       = module.ec2_bastion_host.id
+}
+
+output "ec2_bastion_public_dns" {
+  description = "List of public DNS names assigned to the instances"
+  value       = module.ec2_bastion_host.public_dns
+}
+
+output "ec2_bastion_host_private_ip" {
+  description = "Bastion Host Private IP"
+  value       = module.ec2_bastion_host.private_ip
+}
+
+output "ec2_bastion_host_public_ip" {
+  description = "Bastion Host Public IP"
+  value       = module.ec2_bastion_host.public_ip
+}
+
+################################################################################
+# Security Group Info
+################################################################################
+output "sg_bastion_host_sg_id" {
+  description = "Bastion Security Group ID"
+  value       = module.bastion_host_sg.this_security_group_id
+}
+
+output "sg_bastion_host_sg_name" {
+  description = "Bastion Security Group Name"
+  value       = module.bastion_host_sg.this_security_group_name
 }
