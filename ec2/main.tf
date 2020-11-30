dc1b5c450f31ffc5c3b65880a188ff58dbc3d63b
Tom McLaughlin <ttom.mclaughlin@gmail.com>
2020-11-30 15:58:00 -0600

    Create ec2 in private subnet

--- ec2/main.tf
+++ ec2/main.tf
@@ -31,6 +31,12 @@ data "aws_subnet_ids" "public" {
   }
 }
 
+# Public Subnet info, using ids from data.aws_subnet_ids above
+data "aws_subnet" "public" {
+  for_each = data.aws_subnet_ids.public.ids
+  id       = each.value
+}
+
 ################################################################################
 # Look up latest Amazon Linux AMI
 ################################################################################
@@ -107,5 +113,49 @@ module "bastion_host_sg" {
     "Environment" = "dev"
     "Tier"        = "public"
     "Type"        = "bastion"
+  }
+}
+
+################################################################################
+# Security Group For App Servers
+################################################################################
+module "app_host_sg" {
+  source = "github.com/terraform-aws-modules/terraform-aws-security-group/modules/ssh"
+
+  name            = "app"
+  use_name_prefix = false
+  vpc_id          = data.aws_vpc.selected.id
+
+  ingress_cidr_blocks = [for s in data.aws_subnet.public: s.cidr_block]
+
+  tags = {
+    "Environment" = "dev"
+    "Tier"        = "public"
+    "Type"        = "bastion"
+  }
+}
+
+
+
+################################################################################
+# Create a regular linux host, place in private subnet
+################################################################################
+module "ec2_app_host" {
+  depends_on     = [module.app_host_sg, aws_key_pair.master_key]
+  source         = "github.com/terraform-aws-modules/terraform-aws-ec2-instance?ref=v2.15.0"
+  instance_count = 1
+
+  name                        = "${data.aws_vpc.selected.tags["Name"]}-app"
+  ami                         = data.aws_ami.amazon_linux.id
+  instance_type               = "t2.micro"
+  subnet_ids                  = data.aws_subnet_ids.private.ids
+  vpc_security_group_ids      = [module.app_host_sg.this_security_group_id]
+  associate_public_ip_address = false
+  key_name                    = aws_key_pair.master_key.key_name
+
+  tags = {
+    "Environment" = "dev"
+    "Tier"        = "private"
+    "Type"        = "app"
   }
 }
