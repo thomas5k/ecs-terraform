e1a77167f69ae21a0e9faa867d94350751c34401
Tom McLaughlin <tom.mclaughlin@gmail.com>
2020-11-30 18:58:48 -0600

    Use SSH Rule instead of module

--- ec2/main.tf
+++ ec2/main.tf
@@ -96,36 +96,33 @@ module "bastion_host_sg" {
 ################################################################################
 # Security Group For App Servers (SSH)
 ################################################################################
-module "app_host_ssh_sg" {
-  source = "github.com/terraform-aws-modules/terraform-aws-security-group/modules/ssh"
+# module "app_host_ssh_sg" {
+#   source = "github.com/terraform-aws-modules/terraform-aws-security-group/modules/ssh"
 
-  name            = "${data.aws_vpc.selected.tags["Name"]}-app-ssh"
-  use_name_prefix = false
-  vpc_id          = data.aws_vpc.selected.id
+#   name            = "${data.aws_vpc.selected.tags["Name"]}-app-ssh"
+#   use_name_prefix = false
+#   vpc_id          = data.aws_vpc.selected.id
+
+#   ingress_cidr_blocks = [for s in data.aws_subnet.public: s.cidr_block]
+
+#   tags = {
+#     "Environment" = var.vpc_environment
+#     "Tier"        = "private"
+#     "Type"        = "app"
+#   }
+# }
+
+module "app_host_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+
+  name        = "${data.aws_vpc.selected.tags["Name"]}-app-sg"
+  description = "Allow SSH and HTTP traffic from public subnet CIDR blocks."
+  vpc_id      = data.aws_vpc.selected.id
 
   ingress_cidr_blocks = [for s in data.aws_subnet.public: s.cidr_block]
-
-  tags = {
-    "Environment" = var.vpc_environment
-    "Tier"        = "private"
-    "Type"        = "app"
-  }
-}
-
-module "app_host_http_sg" {
-  source = "github.com/terraform-aws-modules/terraform-aws-security-group/modules/ssh"
-
-  name            = "app-ssh"
-  use_name_prefix = false
-  vpc_id          = data.aws_vpc.selected.id
-
-  ingress_cidr_blocks = [for s in data.aws_subnet.public: s.cidr_block]
-
-  tags = {
-    "Environment" = var.vpc_environment
-    "Tier"        = "public"
-    "Type"        = "bastion"
-  }
+  # Rules are in https://github.com/terraform-aws-modules/terraform-aws-security-group/blob/master/rules.tf
+  ingress_rules       = ["http-80-tcp", "all-icmp", "ssh-tcp"]
+  egress_rules        = ["all-all"]
 }
 
 ################################################################################
@@ -152,10 +149,10 @@ module "ec2_bastion_host" {
 }
 
 ################################################################################
-# EC2 linux host, place in private subnets
+# EC2 linux host, place in private subnet
 ################################################################################
 module "ec2_app_host" {
-  depends_on     = [module.app_host_ssh_sg, aws_key_pair.master_key]
+  depends_on     = [module.app_host_sg, aws_key_pair.master_key]
   source         = "github.com/terraform-aws-modules/terraform-aws-ec2-instance?ref=v2.15.0"
   instance_count = 1
 
@@ -163,7 +160,7 @@ module "ec2_app_host" {
   ami                         = data.aws_ami.amazon_linux.id
   instance_type               = "t2.micro"
   subnet_ids                  = data.aws_subnet_ids.private.ids
-  vpc_security_group_ids      = [module.app_host_ssh_sg.this_security_group_id]
+  vpc_security_group_ids      = [module.app_host_sg.this_security_group_id]
   associate_public_ip_address = false
   key_name                    = aws_key_pair.master_key.key_name
 
